7a662f31c90b938cb9bb6aad371a96457ce86c78
Pongpun364 <pongpun364@hotmail.com>
2021-12-12 00:37:32 +0700

    'done_prod_docker_compose'

---
+++ entrypoint.prod.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if ["$DATABASE" = "postgres" ]
+then 
+    echo "Waitng for postgres ..."
+
+    while != nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+        sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+exec "$@"
